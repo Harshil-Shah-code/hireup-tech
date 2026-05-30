067ffcc50d4e8529168a989c8450c380d89ef84b
Harshil-Shah-code <harshilshah1004@gmail.com>
2026-05-30 22:09:11 +0530

    feat: add AboutCTA component to display contact information and illustration

--- src/components/about/AboutCTA.tsx
+++ src/components/about/AboutCTA.tsx
@@ -26,7 +26,7 @@ export function AboutCTA() {
                       Email us on
                     </div>
                     <div className="text-body-sm font-bold text-[#0A1D37]">
-                      {import.meta.env.VITE_ABOUT_US_SECTION_EMAIL}
+                      {import.meta.env.VITE_INFO_EMAIL_ADDRESS}
                     </div>
                   </div>
                 </div>
